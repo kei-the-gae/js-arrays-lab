b127fc9dc636831e64f8fdc6dc9c2ab1288b92b8
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-06-26 16:10:25 -0700

    Add exercise 7

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ Complete Exercise 5 in the space below:
 
 foods.splice(2, 0, "tofu");
 
-console.log('Exercise 5 result:', foods);
+// console.log('Exercise 5 result:', foods);
 
 /*
 Exercise 6: Replace elements
@@ -74,3 +74,18 @@ Complete Exercise 6 in the space below:
 foods.splice(1, 1, "sushi", "cupcake");
 
 console.log('Exercise 6 result:', foods);
+
+/*
+Exercise 7: Using the `slice()` method
+
+1) Use the `slice()` method to create a new array that contains 'sushi' and 
+   'cupcake'.
+
+2) Assign it to a variable named `yummy`.
+
+Complete Exercise 7 in the space below:
+*/
+
+const yummy = foods.slice(1, 3);
+
+console.log('Exercise 7 result:', yummy);
